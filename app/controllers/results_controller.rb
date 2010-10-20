ef7747ddc9ba7a2a7291f0a4e57155fc64d5b092
jared ning <public@ordinaryzelig.org>
2010-10-20 15:44:26 -0500

    Removed quarter data from table.

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,7 +2,7 @@ class ResultsController < ApplicationController
 
   def index
     @results = Result.sections('Sales').items('EQUAL')
-    @terms = @results.terms_hash
+    @terms = @results.terms_hash.delete_if { |key, val| !['CMTD', 'CYTD'].include?(key) }
     @classes = @results.classes_hash
     @types = @results.types_hash
   end
